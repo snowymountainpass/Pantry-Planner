8438c1e3a37d90f93a8e91728d2d9f4bf07a501e
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-05-29 14:41:16 +0300

    Initial commit

---
+++ src/test/java/com/clockworkcode/pantryplanner/PantryPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clockworkcode.pantryplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
